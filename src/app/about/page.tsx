a7b3c10c62e88b695011d8956b44a597cc989bd1
bert <suckit@outlook.com>
2024-10-18 15:00:56 -0400

    updated about-page, and added a new CSS file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,32 @@
+import './aboutstyle.css';
 export default function About() {
     return (
         <div>
-            <h1>About Us</h1>
-            <p>This is the about page of our Next.js app.</p>
+            <h1><strong>Who We Are</strong></h1>
+
+            <p><br />An Asian cuisine restaurant, serving a variety of East Asian food.</p>
+
+            <div id="container">
+                <section>
+                    <img src="https://www.worldometers.info/img/flags/ch-flag.gif" alt="China-flag" />
+                    <footer>China</footer>
+                </section>
+
+                <section>
+                    <img src="https://www.worldometers.info/img/flags/th-flag.gif" alt="Thailand-flag" />
+                    <footer>Thailand</footer>
+                </section>
+
+                <section>
+                    <img src="https://www.worldometers.info/img/flags/vm-flag.gif" alt="Vietnam-flag" />
+                    <footer>Vietnam</footer>
+                </section>
+
+                <section>
+                    <img src="https://www.worldometers.info/img/flags/ja-flag.gif" alt="Japan-flag" />
+                    <footer>Japan</footer>
+                </section>
+            </div>
         </div>
     );
 }
